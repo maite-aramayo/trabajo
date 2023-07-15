3c93750411a4ef13f328746f4a84a79332d2e79e
maite-aramayo <maitearamayo90@gmail.com>
2023-07-15 20:48:42 -0300

    agregando metodos

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ app.listen(port, () => console.log('Server up: ' + port) );
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
+app.get('/offers', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/offers.html'));
+});
+
+app.get('/official_stores', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/official_stores.html'));
+});
+
+app.get('/sell', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/sell.html'));
+});
+
+app.get('/assistance', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/assistance.html'));
+});
